a663a048c18e6a6ca942f624189e83b981b3957c
Zayaa <Zayaa@LAPTOP-3OMV3MN2>
2026-02-18 11:47:44 +0200

    My initial commit and added print statements

---
+++ githubproject/src/githubproject/Githubproject.java
@@ -0,0 +1,20 @@
+
+package githubproject;
+
+
+public class Githubproject {
+
+    //What is a string?
+    //String is an array of characters
+    //john
+    //[j][o][h][n]
+    //[x0001][x0002]...ect.
+
+    public static void main(String[] args) {
+        System.out.println("My name is John");
+        System.out.print("I am funny");
+        System.out.println("Clown");
+        System.out.println("This has been fun !");
+    }
+    
+}
